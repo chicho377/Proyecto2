46b2f95ffdc8ba626158d417153aee6a8454872d
chicho377 <morafabricio86@gmail.com>
2024-07-12 01:04:41 -0600

    Agregando procedimientos de gotoxy

--- Ejercicio 2/Codigo/main.cpp
+++ Ejercicio 2/Codigo/main.cpp
@@ -33,6 +33,19 @@ void verGraficoVehiculos();
 void verGraficoLavados();
 void hacer_graficos(int a, int y);
 
+// funcion para mover el cursor a una posicion especifica en la consola
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
+// funcion para cambiar el color del texto
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
 int main(int argc, char** argv) {
 	return 0;
 }
