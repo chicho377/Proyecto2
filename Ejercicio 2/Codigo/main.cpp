4c8efd868c9c033235ed187452da2ccd06f36d36
chicho377 <morafabricio86@gmail.com>
2024-07-12 01:07:13 -0600

    Agregando procedimiento ingresarDatosVehiculos

--- Ejercicio 2/Codigo/main.cpp
+++ Ejercicio 2/Codigo/main.cpp
@@ -46,7 +46,7 @@ void setColor(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-// Funcion para mostrar el menu principal
+// funcion para mostrar el menu principal
 void mostrarMenu() {
     system("cls");
     setColor(11);
@@ -62,6 +62,68 @@ void mostrarMenu() {
     gotoxy(22, 11); cout << "6- Salir";
     setColor(15);
     gotoxy(22, 13); cout << "Seleccione una opcion: ";
+}
+
+// funcion para ingresar los datos de los vehiculos
+void ingresarDatosVehiculos() {
+    char continuar;
+    do {
+        if (numVehiculos >= MAX_VEHICULOS) {
+            gotoxy(22, 15); cout << "Se ha alcanzado el limite maximo de vehiculos.\n";
+            break;
+        }
+
+        Vehiculo& veh = vehiculos[numVehiculos];  // referencia al vehiculo actual
+        system("cls");
+        setColor(11);
+        gotoxy(20, 2); cout << "*******************************";
+        gotoxy(20, 3); cout << "*   Ingresar Datos Vehiculos  *";
+        gotoxy(20, 4); cout << "*******************************";
+        setColor(14);
+        gotoxy(22, 6); cout << "Ingrese el numero de placa: ";
+        cin >> veh.placa;
+        gotoxy(22, 7); cout << "Ingrese la marca: ";
+        cin >> veh.marca;
+        gotoxy(22, 8); cout << "Ingrese el modelo: ";
+        cin >> veh.modelo;
+        gotoxy(22, 9); cout << "Ingrese el tipo de vehiculo (1=Sencillo, 2=4x4, 3=Microbus o Camion): ";
+        cin >> veh.tipoVehiculo;
+        gotoxy(22, 10); cout << "Ingrese el tipo de lavado (1=Solo Lavado, 2=Lavado, Aspirado y Encerado): ";
+        cin >> veh.tipoLavado;
+
+        // calcular el precio basado en el tipo de vehiculo y tipo de lavado
+        switch(veh.tipoVehiculo) {
+            case 1:
+                veh.precio = (veh.tipoLavado == 1) ? 3000 : 5000;
+                break;
+            case 2:
+                veh.precio = (veh.tipoLavado == 1) ? 5000 : 7000;
+                break;
+            case 3:
+                veh.precio = (veh.tipoLavado == 1) ? 7000 : 12000;
+                break;
+            default:
+                gotoxy(22, 12); cout << "Tipo de vehiculo invalido.\n";
+                continue;
+        }
+
+        // mostrar los datos ingresados
+        setColor(15);
+        gotoxy(22, 13); cout << "Factura #" << numFactura++;
+        gotoxy(22, 14); cout << "Placa: " << veh.placa;
+        gotoxy(22, 15); cout << "Marca: " << veh.marca;
+        gotoxy(22, 16); cout << "Modelo: " << veh.modelo;
+        gotoxy(22, 17); cout << "Tipo de vehiculo: " << ((veh.tipoVehiculo == 1) ? "Sencillo" : (veh.tipoVehiculo == 2) ? "4x4" : "Microbus o Camion");
+        gotoxy(22, 18); cout << "Tipo de lavado: " << ((veh.tipoLavado == 1) ? "Solo Lavado" : "Lavado, Aspirado y Encerado");
+        gotoxy(22, 19); cout << "Precio a pagar: " << veh.precio << " colones";
+
+        numVehiculos++;
+
+        // preguntar si desea continuar ingresando mas vehiculos
+        setColor(14);
+        gotoxy(22, 21); cout << "Desea continuar? (S/N): ";
+        cin >> continuar;
+    } while (continuar == 'S' || continuar == 's');
 }
 
 int main(int argc, char** argv) {
